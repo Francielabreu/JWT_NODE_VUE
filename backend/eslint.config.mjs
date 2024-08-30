233c584aea43051b8143063a978ed7a38e41e884
Franciel <francielabreu23@gmail.com>
2024-08-30 09:05:43 -0300

    Install do eslint para analise de erros

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -2,8 +2,12 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 
+
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+
+  
 ];
+
